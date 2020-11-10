8945f8c9de49af617b81c8e15f992c8666ced93d
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2020-11-10 11:37:05 +0100

    first commit NodeJS Mise en place de certains fichiers pour Node

--- controller/routeur.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Voici notre routeur, c'est lui qui va orienter le visiteur en fonction de ce qu'il recherche
- * Tout visiteur passera par ce fichier et sera rediriger vers la bonne vue en fonction de ce qu'il recherche
- */
