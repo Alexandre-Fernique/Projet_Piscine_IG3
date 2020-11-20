8db0bbf992652d0e72e966d44e2bd76f9f62e54c
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2020-11-20 09:57:03 +0100

    header.html : Header générique index.html : Insertion de code pour entrer l'en-tête générique index.js : Modification de la fonction de redirection pour intégrer l'entête dans la page hmtl

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.status(200).sendFile(__dirname +  '/view/index.html');
+
+    fs.readFile(__dirname +  '/view/index.html', (err, template) => {
+        if (err)
+            throw err;
+        fs.readFile(__dirname +  '/view/header.html', (err, header) => {
+            if (err)
+                throw err;
+            const html = template.toString().replace("<header>%</header>", header.toString());
+            res.end(html);
+        });
+    });
+
+    //res.status(200).sendFile(__dirname +  '/view/index.html'); -> Pour envoyer un fichier html
     //res.render('index', { title: 'Express' }); -> Si on veut utiliser pug
 });
 
