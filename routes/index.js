320572780a9222ffc1d2f67c2a8b9af80b5dae83
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2020-11-20 11:10:38 +0100

    Ajout d'une page de connexion, qui est la page par défaut lors de l'arrivée d'un utilisateur

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,34 @@ const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-    fs.readFile(__dirname +  '/view/index.html', (err, template) => {
+    fs.readFile(__dirname +  '/view/accueil/connexion.html', (err, template) => { //Page de connexion -> Utilisateur non connecté
         if (err)
             throw err;
-        fs.readFile(__dirname +  '/view/header.html', (err, header) => {
-            if (err)
-                throw err;
-            const html = template.toString().replace("<header>%</header>", header.toString());
-            res.end(html);
-        });
+        res.end(template)
     });
+    //res.status(200).sendFile(__dirname +  '/view/connexion.html'); -> Pour envoyer un fichier html
+    //res.render('index', { title: 'Express' }); -> Si on veut utiliser pug
+});
 
-    //res.status(200).sendFile(__dirname +  '/view/index.html'); -> Pour envoyer un fichier html
+router.get('/inscrit', function(req, res, next) {
+    fs.readFile(__dirname +  '/view/accueil/inscription.html', (err, template) => { //Page de connexion -> Utilisateur non connecté
+        if (err)
+            throw err;
+        res.end(template)
+    });
+    //res.status(200).sendFile(__dirname +  '/view/connexion.html'); -> Pour envoyer un fichier html
     //res.render('index', { title: 'Express' }); -> Si on veut utiliser pug
 });
 
 module.exports = router;
+
+//fs.readFile(__dirname +  '/view/connexion.html', (err, template) => {
+//    if (err)
+//      throw err;
+//  fs.readFile(__dirname +  '/view/header.html', (err, header) => {
+//      if (err)
+//          throw err;
+//      const html = template.toString().replace("<header>%</header>", header.toString());
+//      res.end(html);
+//  });
+//});
