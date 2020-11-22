2bf201c07ac76e15c6e1803b33045a6737f7c7f7
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2020-11-22 18:21:39 +0100

    Passage à mysql Ajout d'un script de base de données

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var etudiantRouter = require('./routes/etudiant');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 // www.exemple.fr/user
 app.use('/', indexRouter); //localhost:3000/
 app.use('/users', usersRouter); //localhost:3000/users
-app.use('/etudiant', etudiantRouter);//localhost:3000/etudiant
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,38 @@ router.get('/', function(req, res, next) {
             throw err;
         res.end(template)
     });
+    //res.status(200).sendFile(__dirname +  '/view/connexion.html'); -> Pour envoyer un fichier html
+    //res.render('index', { title: 'Express' }); -> Si on veut utiliser pug
+});
+
+router.get('/inscrit', function(req, res, next) {
+    fs.readFile(__dirname +  '/view/accueil/inscription.html', (err, template) => { //Page de connexion -> Utilisateur non connecté
+        if (err)
+            throw err;
+        res.end(template)
+    });
+    //res.status(200).sendFile(__dirname +  '/view/connexion.html'); -> Pour envoyer un fichier html
+    //res.render('index', { title: 'Express' }); -> Si on veut utiliser pug
+});
+router.get('/user', function(req, res, next) {
+    fs.readFile(__dirname +  '/view/User/Accueil_User.html', (err, template) => { //Page de connexion -> Utilisateur non connecté
+        if (err)
+            throw err;
+        res.end(template)
+    });
+    //res.status(200).sendFile(__dirname +  '/view/connexion.html'); -> Pour envoyer un fichier html
+    //res.render('index', { title: 'Express' }); -> Si on veut utiliser pug
 });
 
 module.exports = router;
+
+//fs.readFile(__dirname +  '/view/connexion.html', (err, template) => {
+//    if (err)
+//      throw err;
+//  fs.readFile(__dirname +  '/view/header.html', (err, header) => {
+//      if (err)
+//          throw err;
+//      const html = template.toString().replace("<header>%</header>", header.toString());
+//      res.end(html);
+//  });
+//});
