19e56ca7a26c55d7298286a5f4809163f727e4ee
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2020-11-22 18:17:47 +0100

    Passage à mysql Ajout d'un script de base de données

--- bin/bdd.js
+++ bin/bdd.js
@@ -1,42 +1,27 @@
 /**
  * Ce fichier gère la connexion à la base de donnée
  */
-const { Pool } = require('pg');
+const mysql = require('mysql');
+
 
 /*
  * Création d'une instance pour communiquer avec la base de données
  */
-const pool = new Pool({
-    //Nom d'utilisateur de connexion
-    user: "postgres",
-    //Hôte de connexion : localhost -> En local
-    host: "localhost",
-    //Nom de la base de données
-    database: "postgres",
-    //Mot de passe de superutilisateur
-    password: "123456",
-    //Numéro du port de connexion à la BDD
-    port: 5432
-});
-// Mise en place de la connexion à la base de données
-pool.connect();
 
-//id SERIAL PRIMARY KEY, firstname TEXT, lastname TEXT, age INT NOT NULL, address VARCHAR(255), email VARCHAR(50)
-var creation_promotions = "CREATE TABLE promotions(id SERIAL PRIMARY KEY, anneePromotion VARCHAR(15));";
-var suppression_promotions = "DROP TABLE public.promotions;";
-// Ce script ne sera a exectuer que lors du premier lancement de l'application car il détruit les tables puis les créent
-pool.query(suppression_promotions , (err, res) => { // On essaie de supprimer la table promotion
-    console.log("Table de promotions supprimées");
-    pool.query(creation_promotions , (err, res) => { // Puis on créé la table de promotion
-            if (err)
-                throw err;
-            console.log("Table de promotions créées");
-            pool.end();
-        }
-    );
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "projet_piscine"
+});
+
+db.connect(function(err) {
+    if (err)
+        throw err;
+    console.log("Connecté à la base de données MySQL!") ;
 });
 
 // Si on arrive là alors la conenxion s'est bien passée
 console.log("Connexion réussie à la base de données");
 
-module.exports = pool;
+module.exports = mysql;
---
+++ bin/script.sql
@@ -0,0 +1,171 @@
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `composer`
+--
+
+DROP TABLE IF EXISTS `composer`;
+CREATE TABLE IF NOT EXISTS `composer` (
+  `idGroupe` int(11) NOT NULL,
+  `numeroEtudiant` int(11) NOT NULL,
+  PRIMARY KEY (`idGroupe`,`numeroEtudiant`),
+  KEY `fk_numeroEtudiant_etudiants` (`numeroEtudiant`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `creneaux`
+--
+
+DROP TABLE IF EXISTS `creneaux`;
+CREATE TABLE IF NOT EXISTS `creneaux` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `heureDebut` time NOT NULL,
+  `salle` varchar(6) NOT NULL,
+  `idEvenement` int(11) NOT NULL,
+  `idGroupeProjet` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idEvenement` (`idEvenement`),
+  KEY `idGroupeProjet` (`idGroupeProjet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiants`
+--
+
+DROP TABLE IF EXISTS `etudiants`;
+CREATE TABLE IF NOT EXISTS `etudiants` (
+  `numero` int(11) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `prenom` varchar(20) NOT NULL,
+  `mail` varchar(40) NOT NULL,
+  `motDePasse` varchar(64) NOT NULL,
+  `anneePromo` varchar(15) NOT NULL,
+  PRIMARY KEY (`numero`),
+  KEY `anneePromo` (`anneePromo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table ` evenements`
+--
+
+DROP TABLE IF EXISTS ` evenements`;
+CREATE TABLE IF NOT EXISTS ` evenements` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(32) NOT NULL,
+  `dateDebut` date NOT NULL,
+  `Duree` int(11) NOT NULL,
+  `dateLimiteResa` date NOT NULL,
+  `dureeCreneau` time NOT NULL,
+  `nombreMembresJury` int(11) NOT NULL,
+  `anneePromo` varchar(15) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `anneePromo` (`anneePromo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groupeprojet`
+--
+
+DROP TABLE IF EXISTS `groupeprojet`;
+CREATE TABLE IF NOT EXISTS `groupeprojet` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomTuteurEntreprise` varchar(32) DEFAULT NULL,
+  `prenomTuteurEntreprise` varchar(32) DEFAULT NULL,
+  `nomEntreprise` varchar(32) DEFAULT NULL,
+  `idProfesseur` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idProfesseur` (`idProfesseur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participe`
+--
+
+DROP TABLE IF EXISTS `participe`;
+CREATE TABLE IF NOT EXISTS `participe` (
+  `idProfesseur` int(11) NOT NULL,
+  `idCreneaux` int(11) NOT NULL,
+  PRIMARY KEY (`idProfesseur`,`idCreneaux`),
+  KEY `fk_idCreneau_creneaux` (`idCreneaux`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `professeurs`
+--
+
+DROP TABLE IF EXISTS `professeurs`;
+CREATE TABLE IF NOT EXISTS `professeurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(32) NOT NULL,
+  `prenom` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `promotion`
+--
+
+DROP TABLE IF EXISTS `promotion`;
+CREATE TABLE IF NOT EXISTS `promotion` (
+  `annee` varchar(15) NOT NULL,
+  PRIMARY KEY (`annee`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `composer`
+--
+ALTER TABLE `composer`
+  ADD CONSTRAINT `fk_idGroupe_GroupeProjet` FOREIGN KEY (`idGroupe`) REFERENCES `groupeprojet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_numeroEtudiant_etudiants` FOREIGN KEY (`numeroEtudiant`) REFERENCES `etudiants` (`numero`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `creneaux`
+--
+ALTER TABLE `creneaux`
+  ADD CONSTRAINT `fk_idEvenement_evenements` FOREIGN KEY (`idEvenement`) REFERENCES ` evenements` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idGroupeProjet_groupeprojet` FOREIGN KEY (`idGroupeProjet`) REFERENCES `groupeprojet` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `etudiants`
+--
+ALTER TABLE `etudiants`
+  ADD CONSTRAINT `fk_anneePromo_promotion_etudiant` FOREIGN KEY (`anneePromo`) REFERENCES `promotion` (`annee`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table ` evenements`
+--
+ALTER TABLE ` evenements`
+  ADD CONSTRAINT `fk_anneePromo_promotion` FOREIGN KEY (`anneePromo`) REFERENCES `promotion` (`annee`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `groupeprojet`
+--
+ALTER TABLE `groupeprojet`
+  ADD CONSTRAINT `fk_idProfesseur_professeur` FOREIGN KEY (`idProfesseur`) REFERENCES `professeurs` (`id`) ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `participe`
+--
+ALTER TABLE `participe`
+  ADD CONSTRAINT `fk_idCreneau_creneaux` FOREIGN KEY (`idCreneaux`) REFERENCES `creneaux` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idProf_Profeseurs` FOREIGN KEY (`idProfesseur`) REFERENCES `professeurs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
